22b364bc144a83e1e9d796ad2fe5d467077c7451
victordantas1 <victorr.dantas@gmail.com>
2025-09-04 19:06:05 -0400

    feat: use JsonOutputParser and set debug

--- langchain_course/lcel.py
+++ langchain_course/lcel.py
@@ -1,23 +1,36 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.prompts import PromptTemplate
-from langchain_core.output_parsers import StrOutputParser
+from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
+from langchain.globals import set_debug
+from pydantic import Field, BaseModel
 from dotenv import load_dotenv
 
-load_dotenv()
+set_debug(True)
 
-prompt_cidade = PromptTemplate(
-    template="""
-    Sugira uma cidade dado o meu interesse por {interesse}.
-    """,
-    input_variables=['interesse'],
-)
+class Destine(BaseModel):
+    cidade: str = Field("A cidade recomendada para visitar")
+    motivo: str = Field("A motivo pelo qual eh interessante visitar essa cidade")
+
+
+load_dotenv()
 
 model = ChatGoogleGenerativeAI(
     model='gemini-2.5-pro',
     temperature=0.5,
 )
 
-cadeia = prompt_cidade | model | StrOutputParser()
+parser = JsonOutputParser(pydantic_object=Destine)
+
+prompt_cidade = PromptTemplate(
+    template="""
+    Sugira uma cidade dado o meu interesse por {interesse}.
+    {output_format}
+    """,
+    input_variables=['interesse'],
+    partial_variables={'output_format': parser.get_format_instructions()}
+)
+
+cadeia = prompt_cidade | model | parser
 
 resposta = cadeia.invoke({"interesse": "star wars"})
 
